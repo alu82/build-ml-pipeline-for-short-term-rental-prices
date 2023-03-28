7a150a64961929598f73b81306e08aebd778604d
alu82 <ldw_srv@posteo.de>
2023-03-28 23:01:08 +0200

    implemented data cleaning step

--- main.py
+++ main.py
@@ -50,10 +50,19 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            # Download file, perform cleaning, upload cleaned data
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+                "main",
+                parameters={
+                    "input_artifact": "sample.csv:latest",
+                    "output_artifact": "cleaned_sample.csv",
+                    "output_type": "clean_sample",
+                    "output_description": "cleaned data sample",
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"]
+                },
+            )
 
         if "data_check" in active_steps:
             ##################
---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+"""
+Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
+"""
+import argparse
+import logging
+import wandb
+import pandas as pd
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    logger.info("Start to perform basic cleaning with argument: %s", args)
+    run = wandb.init(project="nyc_airbnb", job_type="basic_cleaning")
+    run.config.update(args)
+
+    # Download input artifact. This will also log that this script is using this
+    # particular version of the artifact
+    logger.info("Downloading artifact %s.", args.input_artifact)
+    input_artifact = run.use_artifact(args.input_artifact).file()
+    df = pd.read_csv(input_artifact)
+
+    logger.info("Cleaning data...")
+    idx = df['price'].between(args.min_price, args.max_price)
+    df = df[idx].copy()
+    df['last_review'] = pd.to_datetime(df['last_review'])
+
+    logger.info("Cleaning data complete. Creating and uploading artifact")
+    df.to_csv("clean_data.csv", index=False)
+
+    output_artifact = wandb.Artifact(
+        name = args.output_artifact, 
+        type = args.output_type, 
+        description = args.output_description,
+    )    
+
+    output_artifact.add_file("clean_data.csv")
+    run.log_artifact(output_artifact)
+    logger.info("Artifact added successfully.")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="A very basic data cleaning.")
+
+
+    parser.add_argument(
+        "--input_artifact", 
+        type=str,
+        help="Name of the input artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_artifact", 
+        type=str,
+        help="Name of the output artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_type", 
+        type=str,
+        help="Output artifact type (wandb)",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_description", 
+        type=str,
+        help="Output artifact description (wandb)",
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price", 
+        type=float,
+        help="Minimum price to be considered valid",
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price", 
+        type=float,
+        help="maximum price to be considered valid",
+        required=True
+    )
+
+
+    args = parser.parse_args()
+
+    go(args)
