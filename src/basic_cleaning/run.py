e8b2015f808bbbfd0d01286ee4fc73eb221933db
alu82 <ldw_srv@posteo.de>
2023-03-29 08:57:31 +0200

    added geolocation check to data cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -29,6 +29,9 @@ def go(args):
     df = df[idx].copy()
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("Cleaning data complete. Creating and uploading artifact")
     df.to_csv("clean_data.csv", index=False)
 
